19f9bf9337b0cca4ad2def18b88511f6249fd193
shawn-phy <philipoyuga14@gmail.com>
2024-04-01 15:06:54 +0300

    changed main to show progress to the standard log

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@ int main() {
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
     for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             auto r = double(i) / (image_width-1);
             auto g = double(j) / (image_height-1);
-            auto b = double(i)/(image_width-1) + double(j)/(image_height-1);
+            auto b = 0;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
@@ -24,4 +24,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                 \n";
 }
